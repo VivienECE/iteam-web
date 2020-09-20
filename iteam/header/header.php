3c4fb9da446506bc9996044e2490f18075e37a43
DESKTOP-1V7I5C7\Vivien <vivien.detournay@edu.ece.fr>
2020-09-20 16:41:48 +0200

    Update header.php
    fix css

--- iteam/header/header.php
+++ iteam/header/header.php
@@ -13,8 +13,11 @@
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link href="css/scrolling-nav.css" rel="stylesheet">
-  <link href="css/main.css" rel="stylesheet">
+	<link rel="stylesheet"href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> 
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/scrolling-nav.css">
 
 
 
